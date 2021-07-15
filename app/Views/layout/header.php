ef4284eb41e551a9ac314e6581ba46046b145e74
Pureism <pureismx@gmail.com>
2021-07-15 15:27:01 +0700

    Modulating header and footer

---
+++ app/Views/layout/header.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title><?= $title; ?></title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="/">Kelompok 3</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link" href="/pages/daftarEditor">Editor</a>
+                    <a class="nav-link" href="/pages/daftarPenulis">Penulis</a>
+                    <a class="nav-link" href="/pages/reportProgress">Report Progress</a>
+                    <a class="nav-link" href="/pages/reportOrder">Report Order</a>
+                    <a class="nav-link" href="/pages/reportRoyalty">Report Royalty</a>
+                    <a class="nav-link" href="/pages/about">About</a>
+                </div>
+            </div>
+        </div>
+    </nav>
